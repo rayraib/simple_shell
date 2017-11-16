9315d79fe926a570f19cd349764e4ee26c123a10
kevanlucc <kevan.lucchini@gmail.com>
2017-11-16 23:48:18 +0000

    add modified header and new task

--- shell.h
+++ shell.h
@@ -6,4 +6,13 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+int _strlen(char *s);
+char  *_strcpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char * _strdup(char *s);
+void free_array(char **grid, int height);
+int create_child (char **array);
+int token_count(char *buffer);
+char **create_array(int token_cnt, char *buffer);
+void free_array(char **grid, int height);
 #endif
---
+++ simple_shell_0.2.c
@@ -0,0 +1,43 @@
+#include "shell.h"
+/**
+ * main - UNIX command line interpreter that handles arguments
+ * Return: 0 if Success, -1 if failure
+ */
+int main (void)
+{
+	size_t n = 0;
+	int child_return;
+	int token_cnt;
+	char *buffer = NULL;
+	char **array = NULL;
+
+	while (1)
+	{
+		if (write(1, "sHELL$ ", 7) == -1)
+			perror(" ");
+		if (getline(&buffer, &n, stdin) == -1)
+		{
+			free(buffer);
+			return (-1);
+		}
+		token_cnt = token_count(buffer);
+		array = create_array(token_cnt, buffer);
+		if (array == NULL)
+		{
+			free(buffer);
+			perror(" ");
+			return (-1);
+		}
+		child_return = create_child(array);
+		if (child_return == -1)
+		{
+			free(buffer);
+			free_array(array, token_cnt);
+			perror(" ");
+		}
+		/*free(buffer);*/
+		free_array(array, token_cnt);
+		perror(" ");
+	}
+	return (0);
+}
