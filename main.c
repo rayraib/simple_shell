615067452a5af43d1b27fe94ed479788db247b8f
rayraib <binitarai11@gmail.com>
2017-11-18 22:07:01 +0000

    almost betty compliant

--- main.c
+++ main.c
@@ -9,55 +9,51 @@ int main(void)
 	int token_counter;
 	char **array = NULL;
 	int index;/*, command_ret;*/
-	
+
 	while (1)
 	{
-		buffer = check_input();/*gets arguments from stdin:/BUFFER)*/
+		buffer = check_input();/*gets arguments from stdin*/
 		if (buffer == NULL)
 		{
-			free (buffer);
+			free(buffer);
 			return (-1);
 		}
-		index = find_path();/*index of PATH varible::/BUFFER */
+		index = find_path();/*index of PATH varible*/
 		if (index == -1)
 		{
 			perror("path not found in main ");
 			continue;
 		}
-		first_command = get_command(buffer);/*get the first argument/ BUFFER, FIRST_COMMAND */
+		first_command = get_command(buffer);/*get the first argument*/
 		if (first_command == NULL)
 		{
-			free (buffer);
-			continue;/*no need free FIRST_COMMAND because never exited >> Null)*/
+			free(buffer);
+			continue;
 		}
-		token_counter = token_count(buffer);/*number of arguments pased >> BUFFER, FIRST_COMMAND */
+		token_counter = token_count(buffer);/*number of args passed*/
 		if (token_counter == -1)
 		{
-			free (buffer);
-			free (first_command);
+			free(buffer);
+			free(first_command);
 			printf("something here in main\n");
 		}
-		array = create_array(token_counter, buffer);/*array of pointers to args>> BUFFER, FIRST_cOMMAND, ARRAY*/
+		array = create_array(token_counter, buffer);
+								/*array of pointers to args*/
 		if (array == NULL)
 		{
-			free (buffer);
-			free (first_command);
+			free(buffer);
+			free(first_command);
 			return (-1);
 		}
-		if (not_valid(buffer, first_command, array) == 0)/*it was successfull, command executed */
+		if (not_valid(buffer, first_command, array) == 0)/*command executed */
 		{
-			free (first_command);
-			continue;	
+			free(first_command);
+			continue;
 		}
-		find_command(index, first_command, array);/*find command and execute it>> BUFFER, FIRST_COMMAND, ARRAY*/
-	/*	if (command_ret == -1)
-		{
-			perror(" ");
-		}
-	*/
-		free (buffer);
+		find_command(index, first_command, array);/*find cmmnd & exec*/
+		free(buffer);
 		free_array(token_counter, array);
-		free (first_command);
+		free(first_command);
 	}
 	return (0);
 }
